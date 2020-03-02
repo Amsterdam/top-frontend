ef061ec85c059611a24e886d932a487b307c34fe
Gilles Wittenberg <mail@gilleswittenberg.com>
2020-03-02 13:36:44 +0100

    Fix isPage for home

--- src/config/page.ts
+++ src/config/page.ts
@@ -6,7 +6,7 @@ export const to = (path: string = "") => {
   return `${ basepath }${ path }`
 }
 
-export const isPage = (page: string) => trimTrailingSlash(window.location.pathname) === to(trimTrailingSlash(page))
+export const isPage = (page: string) => trimTrailingSlash(window.location.pathname) === trimTrailingSlash(to(page))
 export const isLoginPage = () => isPage("login")
 export const isLoginCallbackPage = () => isPage("authentication/callback")
 export const isHomePage = () => isPage("")
