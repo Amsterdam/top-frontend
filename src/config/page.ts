e93e0923656116214efa54dc8510b9b5610e5a86
Gilles Wittenberg <mail@gilleswittenberg.com>
2020-03-02 10:55:29 +0100

    Fix trailing slash in paths

--- src/config/page.ts
+++ src/config/page.ts
@@ -1,10 +1,12 @@
+const trimTrailingSlash = (str: string) => str.replace(/\/$/, "")
+
 export const basepath = "/"
 
 export const to = (path: string = "") => {
   return `${ basepath }${ path }`
 }
 
-export const isPage = (page: string) => window.location.pathname === to(page)
+export const isPage = (page: string) => trimTrailingSlash(window.location.pathname) === to(trimTrailingSlash(page))
 export const isLoginPage = () => isPage("login")
 export const isLoginCallbackPage = () => isPage("authentication/callback")
 export const isHomePage = () => isPage("")
