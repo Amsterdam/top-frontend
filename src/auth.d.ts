2b8fa118ab9c15b0f307c5d0eecdeedbc4c4d341
Gilles Wittenberg <mail@gilleswittenberg.com>
2020-03-03 10:44:08 +0100

    Fix type

--- src/auth.d.ts
+++ src/auth.d.ts
@@ -4,7 +4,7 @@ declare type AuthToken = string
 declare type OAuthToken = AuthToken | undefined
 declare type DecodedAuthToken = {
   token_type: string
-  exp: 1583239133
+  exp: number
   jti: string
   user_id: string
 }
