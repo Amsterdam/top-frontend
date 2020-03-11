8c7b251af0feec19faf80f298168e84333fc5490
Gilles Wittenberg <mail@gilleswittenberg.com>
2020-03-11 15:03:49 +0100

    Buttons

--- src/components/global/OptionsButton.tsx
+++ src/components/global/OptionsButton.tsx
@@ -1,10 +1,11 @@
 import React, { FC, useRef } from "react"
 import { Button } from "@datapunt/asc-ui"
 import noop from "../../lib/utils/noop"
+import { Ellipsis } from "@datapunt/asc-assets"
 
 type Props = {
   onClick?: () => void
 }
 
-const OptionsButton: FC<Props> = ({ onClick = noop }) => <Button onClick={ onClick }>&hellip;</Button>
+const OptionsButton: FC<Props> = ({ onClick = noop }) => <Button icon={ <Ellipsis /> } onClick={ onClick }>&hellip;</Button>
 export default OptionsButton
--- src/components/itineraries/AddButton.tsx
+++ src/components/itineraries/AddButton.tsx
@@ -1,7 +1,8 @@
 import React, { FC } from "react"
 import { Link } from "@reach/router"
-import styled from "styled-components"
+import { Enlarge } from "@datapunt/asc-assets"
 import { to } from "../../config/page"
+import styled from "styled-components"
 
 type Props = {
   itineraryId: Id
@@ -22,8 +23,13 @@ const StyledLink = styled(Link)`
   border: 1px solid #004699;
   padding: 12px 15px;
   transition: color 0.1s ease-in-out 0s, background-color 0.1s ease-in-out 0s;
+  svg {
+    width: 18px
+    height: 18px
+    margin-right: 12px
+  }
 `
 
 const AddButton: FC<Props> = ({ itineraryId }) =>
-  <StyledLink to={ to(`suggesties/${ itineraryId }`) }>Voeg adres toe</StyledLink>
+  <StyledLink to={ to(`suggesties/${ itineraryId }`) }><Enlarge /> Voeg adres toe</StyledLink>
 export default AddButton
