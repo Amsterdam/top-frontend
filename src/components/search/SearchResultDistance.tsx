4371439ace606e0b0c3f96f5bd5482e99980a833
Gilles Wittenberg <mail@gilleswittenberg.com>
2020-03-16 11:23:28 +0100

    Display distance in both km and m

--- src/components/search/SearchResultDistance.tsx
+++ src/components/search/SearchResultDistance.tsx
@@ -11,8 +11,10 @@ const P = styled.p`
 `
 
 const SearchResultDistance: FC<Props> = ({ distance }) => {
-  const km = (distance / 1000).toPrecision(2)
-  return <P>{ km } km</P>
+  const displayKm = distance >= 1000
+  const value = displayKm ? (distance / 1000).toPrecision(2) : distance
+  const unit = displayKm ? "km" : "m"
+  return <P>{ value } { unit }</P>
 }
 
 
