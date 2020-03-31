ded43e3b87c57f2b9a02dc8975264c487288dd39
Jeffrey Zutt <jeffrey.zutt@leaseplan.com>
2020-03-31 14:37:51 +0200

    feat: use IconButton instead of button+icon

--- src/components/global/IconButton.tsx
+++ src/components/global/IconButton.tsx
@@ -1,16 +1,20 @@
 import React, { FC, ReactNode, MouseEvent } from "react"
 import { Button } from "@datapunt/asc-ui"
 import StyledButton from "../styled/Button"
-import { Enlarge, Minimise, TrashBin, DocumentText, Ellipsis } from "@datapunt/asc-assets"
+import { Enlarge, Minimise, TrashBin, DocumentText, Ellipsis, Close } from "@datapunt/asc-assets"
 import noop from "../../lib/utils/noop"
 
 type Props = {
-  icon?: "Enlarge" | "Minimise" | "TrashBin" | "DocumentText" | "Ellipsis"
+  icon?: "Enlarge" | "Minimise" | "TrashBin" | "DocumentText" | "Ellipsis" | "Close"
   iconNode?: ReactNode
   onClick?: (a: MouseEvent<HTMLButtonElement>) => void
   size?: number
   border?: boolean
   disabled?: boolean
+
+  // By adding this property we make IconButton stylable through styled-components.
+  // https://styled-components.com/docs/advanced#styling-normal-react-components
+  className?: string
 }
 
 const iconMap = {
@@ -18,13 +22,14 @@ const iconMap = {
   "Minimise": <Minimise />,
   "TrashBin": <TrashBin />,
   "DocumentText": <DocumentText />,
-  "Ellipsis": <Ellipsis />
+  "Ellipsis": <Ellipsis />,
+  "Close": <Close />
 }
 
-const IconButton: FC<Props> = ({ icon, iconNode, onClick = noop, size = 44, border = true, disabled = false }) => {
+const IconButton: FC<Props> = ({ className, icon, iconNode, onClick = noop, size = 44, border = true, disabled = false }) => {
   const Component = border ? StyledButton : Button
   const iconElement = icon !== undefined ? iconMap[icon] : iconNode
-  return <Component onClick={ onClick } size={ size } variant="blank" icon={ iconElement } disabled={ disabled } />
+  return <Component onClick={ onClick } size={ size } variant="blank" icon={ iconElement } disabled={ disabled } className={className} />
 }
 
 export default IconButton
--- src/components/global/Modal/DefaultModal.tsx
+++ src/components/global/Modal/DefaultModal.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import styled from "styled-components";
-import { Close } from "@datapunt/asc-assets"
 import { constants } from "@datapunt/asc-ui"
 
 import Portal from "./Portal";
+import IconButton from "../IconButton";
 
 const FixedWrap = styled.div`  
   z-index: 99999; // HeaderWrap has z-index of 9999, we have top that. 
@@ -27,27 +27,11 @@ const TopBar = styled.div`
   border-bottom: 1px solid #DFDFDF;
 `
 
-// Remove default browser-styling from a button
-const Button = styled.button`
-  display: block
-  cursor: pointer
-  background: none
-  border: none
-  outline: none
-  width: 100%
-  height: 100%
-  padding: 0
-`
-
-const CloseIcon = styled(Close)`
+const StyledIconButton = styled(IconButton)`
   position: absolute;
-  right: 12px;
-  
+  right: 0;
   top: 50%;
-  transform: translateY(-50%);    
-  
-  width: 24px;
-  height: 24px;
+  transform: translateY(-50%);
 `
 
 const ScrollContainer = styled.div`
@@ -73,12 +57,11 @@ const defaultClickHandler = () => {
 const DefaultModal:React.FC<Props> = ({onClick, children}) => (<Portal>
   <FixedWrap>
     <TopBar>
-      <Button onClick={() => onClick
-          ? onClick()
-          : defaultClickHandler()
-      }>
-        <CloseIcon />
-      </Button>
+        <StyledIconButton
+          border={false}
+          icon='Close'
+          onClick={() => onClick ? onClick() : defaultClickHandler()}
+        />
     </TopBar>
     <ScrollContainer>
       <Spacing>
