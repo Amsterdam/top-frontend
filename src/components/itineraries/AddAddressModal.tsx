a7899777325d3b6de7f501145669f970d829b4ba
Jeffrey Zutt <jeffrey.zutt@leaseplan.com>
2020-04-01 16:57:21 +0200

    feat: add search results to add address modal

--- src/components/itineraries/AddAddressModal.tsx
+++ src/components/itineraries/AddAddressModal.tsx
@@ -3,27 +3,43 @@ import styled from 'styled-components'
 import DefaultModal, { defaultCloseHandler } from "../global/Modal/DefaultModal"
 import {Button} from "@datapunt/asc-ui"
 import SearchForm from "../search/SearchForm"
+import useGlobalState from "../../hooks/useGlobalState"
+import SearchResults from "../search/SearchResults"
+import Spinner from "../global/Spinner"
 
 const Div = styled.div`
   padding-bottom: 15px;
 `
 
-const AddAddressModal:React.FC = () => (
-  <DefaultModal
-    title='Startadres'
-    body={<>
-      <Div>
-        <b>Bij welk adres wil je beginnen?</b>
-      </Div>
-      <Div>
-        <SearchForm />
-      </Div>
-    </>}
-    footer={<>
-      <Button variant='primary'>Startadres toevoegen</Button>
-      <Button variant='tertiary' onClick={defaultCloseHandler}>Annuleer</Button>
-    </>}
-  />
-)
+const AddAddressModal:React.FC = () => {
+  const {
+    search: {
+      isFetching,
+      results
+    }
+  } = useGlobalState()
+
+  const showSpinner = isFetching
+
+  return (
+    <DefaultModal
+      title='Startadres'
+      body={<>
+        <Div>
+          <b>Bij welk adres wil je beginnen?</b>
+        </Div>
+        <Div>
+          <SearchForm />
+          { showSpinner && <Spinner /> }
+          <SearchResults results={results} />
+        </Div>
+      </>}
+      footer={<>
+        <Button variant='primary'>Startadres toevoegen</Button>
+        <Button variant='tertiary' onClick={defaultCloseHandler}>Annuleer</Button>
+      </>}
+    />
+  )
+}
 
 export default AddAddressModal
