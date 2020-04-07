e186d4e0feec11d8ec32802519045880c854d415
Jeffrey Zutt <jeffrey.zutt@leaseplan.com>
2020-04-07 08:27:45 +0200

    chore: add blank lines linting rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,8 @@ module.exports = {
     "react-app"
   ],
   rules: {
+    "no-multiple-empty-lines": ["error"],
+    "padded-blocks": ["error", "never"],
     "block-spacing": ["error", "always"],
     "space-unary-ops": ["error"],
     "space-infix-ops": ["error"],
