7fc7f96eae419cc6587459575503d5fccbb4bac8
Gilles Wittenberg <mail@gilleswittenberg.com>
2020-04-07 16:53:14 +0200

    Fix layout of 404 page and add some TODO's

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -1,8 +1,19 @@
 import React from "react"
 import { RouteComponentProps } from "@reach/router"
+import ErrorMessage from "../components/global/ErrorMessage"
+import styled from "styled-components"
 
 type Props = RouteComponentProps
 
-const NotFoundPage: React.FC<Props> = () => <p>404</p>
+const Div = styled.div`
+  margin-top: 200px
+  text-align: center
+`
+
+const NotFoundPage: React.FC<Props> = () => (
+  <Div>
+    <ErrorMessage text="404 Pagina niet gevonden" />
+  </Div>
+)
 
 export default NotFoundPage
