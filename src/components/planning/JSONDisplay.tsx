219ca94722b7837bd314febf1e4a3fe217d1ff81
Gilles Wittenberg <mail@gilleswittenberg.com>
2020-04-14 14:09:57 +0200

    JSON

--- src/components/planning/JSONDisplay.tsx
+++ src/components/planning/JSONDisplay.tsx
@@ -1,5 +1,6 @@
 import React, { FC, useState } from "react"
 import { Button } from "@datapunt/asc-ui"
+import JSONBlock from "../global/JSONBlock"
 
 type Props = {
   json?: Record<string, any>
@@ -14,9 +15,7 @@ const JSONDisplay: FC<Props> = ({ json }) => {
       <h1>Huidige settings (JSON)</h1>
       <Button onClick={ onClickShowJSON }>{ `${ showJSON ? "Verberg" : "Toon" } JSON` }</Button>
       { showJSON &&
-        <div>
-          <pre><code>{ JSON.stringify(json, null, 2) }</code></pre>
-        </div>
+        <JSONBlock json={ json } />
       }
     </div>
   )
