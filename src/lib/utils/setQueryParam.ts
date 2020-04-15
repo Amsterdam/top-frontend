a6f221498bc72ce308408b04a17afe173f4b5f8a
Jeffrey Zutt <jeffrey.zutt@leaseplan.com>
2020-04-15 13:38:48 +0200

    refactor: a little more friendly query string management

--- src/lib/utils/setQueryParam.ts
+++
@@ -1,7 +0,0 @@
-import parseLocationSearch from "./parseLocationSearch"
-import queryParams from "./queryParams"
-
-export const setQueryParam = (search:string, param:string, value:string) => queryParams({
-    ...parseLocationSearch(search),
-    [param]: value
-  })
