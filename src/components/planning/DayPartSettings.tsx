afb98849bfcf9da9aca9447f8cacc431b756134b
Jeffrey Zutt <jeffrey.zutt@leaseplan.com>
2020-04-23 16:24:17 +0200

    fix: validators in daypart settings

--- src/components/planning/DayPartSettings.tsx
+++ src/components/planning/DayPartSettings.tsx
@@ -4,6 +4,7 @@ import { AccordionWrapper, Accordion } from "@datapunt/asc-ui"
 import { StadiumSettings } from "./StadiumSettings"
 import { isNotIntersectingWith } from "../form-components/validators/isNotIntersectingWith"
 import SelectField from "../form-components/SelectField"
+import { combineValidators } from "../form-components/validators/combineValidators"
 
 type Props = {
   title: string,
@@ -11,10 +12,6 @@ type Props = {
   dayPart: DayPart,
   stadia: Stadia
 }
-
-const StyledAccordionWrapper = styled(AccordionWrapper)`
-  
-`
 
 const StyledAccordion = styled(Accordion)`
   // UX wants the accordion headers bold.  
@@ -34,14 +31,17 @@ const DayPartSettings:React.FC<Props> = ({ title, day, dayPart, stadia }) => {
   const primaryStadium = getFieldName(day, dayPart, "primary_stadium")
   const secondaryStadia = getFieldName(day, dayPart, "secondary_stadia")
   const excludeStadia = getFieldName(day, dayPart, "exclude_stadia")
-
   const options = stadia.reduce((acc, stadium) => ({ ...acc, [stadium]:stadium }), { "": "Geen" })
 
   return (
     <AccordionWrapper>
       <StyledAccordion title={title} isOpen={dayPart !== "evening"}>
       <H4>1. Zoveel mogelijk</H4>
-      <SelectField name={primaryStadium} options={options} />
+      <SelectField
+        name={primaryStadium}
+        options={options}
+        validate={isNotIntersectingWith(excludeStadia)}
+      />
       <H4>2. Aanvullen met</H4>
       <StadiumSettings
         fieldName={secondaryStadia}
@@ -52,7 +52,12 @@ const DayPartSettings:React.FC<Props> = ({ title, day, dayPart, stadia }) => {
       <StadiumSettings
         fieldName={excludeStadia}
         stadia={stadia}
-        validate={isNotIntersectingWith(secondaryStadia)}
+        validate={
+          combineValidators(
+            isNotIntersectingWith(primaryStadium),
+            isNotIntersectingWith(secondaryStadia)
+          )
+        }
       />
       </StyledAccordion>
     </AccordionWrapper>
