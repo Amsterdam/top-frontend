b772086e8ea04ccf4373a7a926fe34df2ff31ef0
Gilles Wittenberg <mail@gilleswittenberg.com>
2020-04-24 12:14:35 +0200

    ESLint no multi spacing

--- src/components/cases/FraudPrediction/ShapValue.tsx
+++ src/components/cases/FraudPrediction/ShapValue.tsx
@@ -19,7 +19,7 @@ type BarProps = Pick<Props, "shap" | "isPositive">
 const Bar = styled.div`
   width: 0;
   
-  background-color: ${ ({ isPositive }: BarProps) =>  color( isPositive ? "primary" : "secondary") }; 
+  background-color: ${ ({ isPositive }: BarProps) => color( isPositive ? "primary" : "secondary") }; 
   border-radius: 3px; 
   height: 10px; 
   
