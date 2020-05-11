21605e78176a4e31fb1643d35ad9822153bb315c
Gilles Wittenberg <mail@gilleswittenberg.com>
2020-05-11 16:29:42 +0200

    Use AuthActions type again

--- src/components/providers/StateProvider/hooks/useCreateAuthenticateFunction.tsx
+++ src/components/providers/StateProvider/hooks/useCreateAuthenticateFunction.tsx
@@ -1,7 +1,7 @@
 import { useCallback } from "react"
 import { StateContextInitializer } from "./useCreateInitializeFunction"
 
-const useCreateAuthenticateFunction = (authActions: { authenticate: (a: AuthToken, b: AuthUser) => boolean }, initialize: StateContextInitializer) => {
+const useCreateAuthenticateFunction = (authActions: AuthActions, initialize: StateContextInitializer) => {
   const authenticate = (token: AuthToken, user: AuthUser) => {
     const isSuccess = authActions.authenticate(token, user)
     if (isSuccess) initialize()
