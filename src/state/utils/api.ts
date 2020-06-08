18696f9c7d6d7e0cb8c3d9a19f425c8bd886d24e
Gilles Wittenberg <mail@gilleswittenberg.com>
2020-06-08 10:16:59 +0200

    Clean up

--- src/state/utils/api.ts
+++ src/state/utils/api.ts
@@ -1,5 +1,4 @@
 import config from "../../config/api"
 
 const { domain, apiPath: path } = config
-//export default { domain, path, suffix: "" } as APIConfig
 export default { domain, path, suffix: "" }
