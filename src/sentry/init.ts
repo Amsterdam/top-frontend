15a5e454dcb9d64db2f1f324446895b1f90d1512
Gilles Wittenberg <mail@gilleswittenberg.com>
2020-06-11 11:59:12 +0200

    Check environment

--- src/sentry/init.ts
+++ src/sentry/init.ts
@@ -1,4 +1,15 @@
 import * as Sentry from "@sentry/browser"
 import { dsn } from "../config/sentry"
 
-export default () => Sentry.init({ dsn })
+const environment = process.env.NODE_ENV
+const release = process.env.GIT_COMMIT
+
+export default () => {
+  if (environment !== "development") {
+    Sentry.init({
+      dsn,
+      environment,
+      release
+    })
+  }
+}
