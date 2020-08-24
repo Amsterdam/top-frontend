66a551758760ae020cf7113cec22628a108690f8
Gilles Wittenberg <mail@gilleswittenberg.com>
2020-08-24 16:38:38 +0200

    Styling HelpButton

--- src/app/features/shared/components/molecules/HelpIcon/HelpButton.tsx
+++ src/app/features/shared/components/molecules/HelpIcon/HelpButton.tsx
@@ -10,10 +10,11 @@ import styled from "styled-components"
 const GUTTER = 8
 const HELP_TEXT_WIDTH = 320
 
+const DIM = 28
 const Wrap = styled.div`
   position: relative;
-  width: 18px;
-  height: 18px;
+  width: ${ DIM }px;
+  height: ${ DIM }px;
 `
 
 const Overlay = styled.div`
@@ -22,7 +23,7 @@ const Overlay = styled.div`
   left: 0;
   bottom: 0;
   right: 0;
-  z-index: 100;   
+  z-index: 100;
   background-color: rgba(0,0,0,.4);
 `
 
@@ -40,10 +41,16 @@ const HelpText = styled.div<HelpTextProps>`
   border: 1px solid ${ themeColor("tint", "level2") };
   background-color: ${ themeColor("tint", "level1") };
   z-index: 500;
-  
+
   width: ${ HELP_TEXT_WIDTH }px;
-  left: ${ props => props?.leftOffset !== undefined ? -HELP_TEXT_WIDTH + props.leftOffset : -HELP_TEXT_WIDTH }px;  
-  ${ props => props.top !== undefined && `top: ${ props.top + GUTTER }px;` }    
+  left: ${ props => props?.leftOffset !== undefined ? -HELP_TEXT_WIDTH + props.leftOffset : -HELP_TEXT_WIDTH }px;
+  ${ props => props.top !== undefined && `top: ${ props.top + GUTTER }px;` }
+`
+
+const StyledButton = styled(Button)`
+  padding: 12px;
+  border: 2px solid ${ themeColor("tint", "level7") };
+  border-radius: 50%;
 `
 
 const HelpButton: React.FC = ({ children }) => {
@@ -60,7 +67,7 @@ const HelpButton: React.FC = ({ children }) => {
   return (
     <Wrap>
       <ButtonWrap ref={buttonRef}>
-        <Button size={18} variant="blank" onClick={handleClick}>?</Button>
+        <StyledButton size={18} variant="blank" onClick={handleClick}>?</StyledButton>
       </ButtonWrap>
       { isOpen && (
         <>
