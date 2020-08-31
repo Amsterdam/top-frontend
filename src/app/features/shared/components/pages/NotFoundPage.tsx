29fc29e69ccd51b503d3a61cac9e3fa9032d70f0
Jeffrey Zutt <jeffrey.zutt@leaseplan.com>
2020-08-31 14:35:29 +0200

    fix: add 404 page

---
+++ src/app/features/shared/components/pages/NotFoundPage.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+import { RouteComponentProps } from "@reach/router"
+import DefaultLayout from "../layouts/DefaultLayout/DefaultLayout";
+import ErrorMessage from "../atoms/ErrorMessage/ErrorMessage";
+
+const NotFoundPage:React.FC<RouteComponentProps> = () => (
+  <DefaultLayout>
+    <ErrorMessage text="404 Pagina niet gevonden" />
+  </DefaultLayout>
+)
+
+export default NotFoundPage
