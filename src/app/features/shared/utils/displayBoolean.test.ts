0f0cb315859651ce0031a3a9994d9a14a0693084
Gilles Wittenberg <mail@gilleswittenberg.com>
2020-09-09 11:31:03 +0200

    Add deleted test

---
+++ src/app/features/shared/utils/displayBoolean.test.ts
@@ -0,0 +1,13 @@
+import displayBoolean from "./displayBoolean"
+
+describe("displayBoolean", () => {
+  it("undefined", () => {
+    expect(displayBoolean(undefined)).toBe("Nee")
+  })
+  it("false", () => {
+    expect(displayBoolean(false)).toBe("Nee")
+  })
+  it("true", () => {
+    expect(displayBoolean(true)).toBe("Ja")
+  })
+})
