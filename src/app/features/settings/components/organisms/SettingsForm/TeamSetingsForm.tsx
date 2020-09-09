fa34ed67d1ffcd544b651a77992bb1402ccef0e9
Maurice Guikema <mguikema@gmail.com>
2020-09-09 17:17:47 +0200

    feat: team settings update

--- src/app/features/settings/components/organisms/SettingsForm/TeamSetingsForm.tsx
+++ src/app/features/settings/components/organisms/SettingsForm/TeamSetingsForm.tsx
@@ -1,8 +1,8 @@
 import React, { FC, useCallback, useMemo, useState } from "react"
-import { RouteComponentProps } from "@reach/router"
+import { RouteComponentProps, Link } from "@reach/router"
 import { ScaffoldForm } from "amsterdam-react-final-form"
 import styled from "styled-components"
-
+import to from "app/features/shared/routing/to"
 import { useProjectConstants, useTeamSettings, useStadiaConstants } from "app/state/rest"
 
 import Scaffold from "app/features/shared/components/form/Scaffold"
@@ -17,8 +17,8 @@ const Wrap = styled.div`
   margin: 0 8px 100px 8px
 `
 
-const filterEmptyPostalCodes = (settings: Components.Schemas.TeamSettings) =>
-  ({ ...settings, postal_codes: settings.settings.postal_codes?.filter(i => i != null) ?? []}
+const filterEmptyPostalCodes = (settings: any) =>
+  ({ ...settings, postal_codes: settings.postal_codes?.filter((i: any) => i != null) ?? []}
 )
 
 type Props = {
@@ -26,7 +26,7 @@ type Props = {
 }
 
 const TeamSettingsForm: FC<RouteComponentProps<Props>> = ({ teamSettingsId }) => {
-  const { data: settings, execPost, isBusy: isBusySettings } = useTeamSettings(teamSettingsId!)
+  const { data: settings, execPut, isBusy: isBusySettings } = useTeamSettings(teamSettingsId!)
   const { data: projects, isBusy: isBusyProjects } = useProjectConstants()
   const { data: stadia, isBusy: isBusyStadia } = useStadiaConstants()
 
@@ -37,23 +37,30 @@ const TeamSettingsForm: FC<RouteComponentProps<Props>> = ({ teamSettingsId }) =>
     [ projects, stadia ]
   )
 
-  const handleSubmit = useCallback(async (settings: Components.Schemas.TeamSettings) => {
-    const values = filterEmptyPostalCodes(settings)
+  const handleSubmit = useCallback(async (settings: any) => {
+    const values = filterEmptyPostalCodes(settings.settings)
     setErrorMessage("")
 
     try {
-      await execPost(values, { skipCacheClear: true, useResponseAsCache: true })
+      await execPut({settings: values, name: settings.name}, { skipCacheClear: true, useResponseAsCache: true })
     } catch(error) {
       setErrorMessage(error.response.data.message)
       return error
     }
-  }, [ execPost, setErrorMessage ])
+  }, [ execPut, setErrorMessage ])
 
   if (!settings || isBusySettings || isBusyProjects || isBusyStadia) { return <CenteredSpinner size={60} /> }
 
   return <DefaultLayout>
     <Wrap>
-      <ScaffoldForm onSubmit={handleSubmit} initialValues={settings}>
+      <h2>Settings details: {settings.name}</h2>
+      <Link to={to("/team-settings")}>
+        Alle instellingen
+      </Link>
+      <ScaffoldForm onSubmit={handleSubmit} initialValues={{
+          name: settings.name,
+          settings: settings.settings,
+      }}>
         <Scaffold {...definition} />
         <FixedSubmitButton errorMessage={errorMessage} />
         <JSONDisplay />
