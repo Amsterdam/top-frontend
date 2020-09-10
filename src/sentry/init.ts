7ccd1e72d1c8024ba481a0e7c9ff976b15929dfc
Jeffrey Zutt <jeffrey.zutt@leaseplan.com>
2020-09-10 10:53:05 +0200

    feat: pass commit hash to sentry

--- src/sentry/init.ts
+++ src/sentry/init.ts
@@ -2,10 +2,10 @@ import * as Sentry from "@sentry/browser"
 
 export default () => {
   if (process.env.REACT_APP_SENTY_ENV && process.env.REACT_APP_SENTY_ENV !== "development") {
-    // @TODO: Pass Git commit hash as third argument
     Sentry.init({
       dsn: "https://889f135ead3a4523aa463e38e87e6a92@sentry.data.amsterdam.nl/47",
-      environment: process.env.REACT_APP_SENTY_ENV
+      environment: process.env.REACT_APP_SENTY_ENV,
+      release: process.env.REACT_APP_GIT_COMMIT_HASH
     })
   }
 }
