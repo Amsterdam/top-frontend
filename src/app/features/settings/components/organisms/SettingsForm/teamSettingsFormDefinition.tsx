00dbc6c718a2112a87c5ba3a14f0476e6b8b0f9b
Maurice Guikema <mguikema@gmail.com>
2020-09-29 13:37:20 +0200

    TOP-34: team settings name removed from form

--- src/app/features/settings/components/organisms/SettingsForm/teamSettingsFormDefinition.tsx
+++ src/app/features/settings/components/organisms/SettingsForm/teamSettingsFormDefinition.tsx
@@ -77,15 +77,6 @@ export const createDefinition = (projects: string[], stadia: string[]) => {
   const postalCodeMin = 1000
   const postalCodeMax = 1109
   const definition: FormPositionerFields<Field> = {
-    name: {
-        type: "TextField",
-        props: {
-          label: "Naam van de instelling",
-          name: "name",
-          type: "text",
-          validate: isRequired()
-        }
-      },
     opening_date: {
       type: "TextField",
       props: {
@@ -168,7 +159,6 @@ export const createDefinition = (projects: string[], stadia: string[]) => {
     // 3 columns:
     .setGrid("laptop", "1fr 1fr 1fr", [
       /* eslint-disable no-multi-spaces */
-      [ "name"                                                        ],
       [ "opening_date"                                                ],
       [ "postal_codes",       "postal_codes",     "postal_codes"      ],
       [ "projects",           "projects",         "projects"          ],
@@ -182,7 +172,6 @@ export const createDefinition = (projects: string[], stadia: string[]) => {
     // 5 columns
     .setGrid("laptopL", "1fr 1fr 1fr 1fr 1fr", [
       /* eslint-disable no-multi-spaces */
-      [ "name"                                                                                                ],
       [ "opening_date"                                                                                        ],
       [ "postal_codes",       "postal_codes",     "postal_codes"                                              ],
       [ "projects",           "projects",         "projects",           "projects",         "projects"        ],
