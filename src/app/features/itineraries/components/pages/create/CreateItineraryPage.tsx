b7950132c1537c203be732cf783f6625226d9ca5
Vincent Smedinga <vincent.smedinga@informaat.nl>
2020-10-06 15:56:48 +0200

    TOP-34: Update titles for ‘Create itinerary’ page

--- src/app/features/itineraries/components/pages/create/CreateItineraryPage.tsx
+++ src/app/features/itineraries/components/pages/create/CreateItineraryPage.tsx
@@ -1,16 +1,19 @@
 import React, { useEffect } from "react"
-import { RouteComponentProps, navigate } from "@reach/router"
+import { navigate, RouteComponentProps } from "@reach/router"
+
+import { Heading } from "@datapunt/asc-ui";
 
 import { useItineraries, useTeamSettingsList } from "app/state/rest"
+import { useLoggedInUser } from "app/state/rest/custom/useLoggedInUser"
 import { useTeamSettings } from "app/state/rest/custom/useTeamSettings"
 
+import Spacing from "app/features/shared/components/atoms/Spacing/Spacing";
+import CenteredSpinner from "app/features/shared/components/atoms/CenteredSpinner/CenteredSpinner"
 import DefaultLayout from "app/features/shared/components/layouts/DefaultLayout/DefaultLayout"
 import { useQueryString } from "app/features/shared/hooks/queryString/useQueryString"
 import to from "app/features/shared/routing/to"
-import CenteredSpinner from "app/features/shared/components/atoms/CenteredSpinner/CenteredSpinner"
 
 import ItineraryForm from "app/features/itineraries/components/organisms/ItineraryForm/ItineraryForm"
-import { useLoggedInUser } from "app/state/rest/custom/useLoggedInUser"
 
 type Props = {
   teamSettingsId: number
@@ -28,31 +31,40 @@ const CreateItineraryPage: React.FC<RouteComponentProps<Props>> = ({ teamSetting
     if (shouldRedirect) {
       navigate(to("/lijst/:itineraryId/", { itineraryId: data?.itineraries[0].id.toString() }))
     }
-  }, [shouldRedirect, data])
+  }, [ shouldRedirect, data ])
 
   if (!teamSettingsList || teamSettingsList.length <= 0) {
     return null
   }
-  
+
   // @ts-ignore
-  if (!teamSettings && loggedInUser?.team_settings.length > 0 && teamSettingsList.length > 0){
-    teamSettingsList = teamSettingsList.filter((cur) => 
-    cur.id === loggedInUser?.team_settings[0].id
+  if (!teamSettings && loggedInUser?.team_settings.length > 0 && teamSettingsList.length > 0) {
+    teamSettingsList = teamSettingsList.filter((cur) =>
+      cur.id === loggedInUser?.team_settings[0].id
     )
     teamSettings = teamSettingsList[0]
   }
 
-  if (!teamSettings){
+  if (!teamSettings) {
     return null
   }
 
-  return <DefaultLayout>
-    <h1>{teamSettings.name}</h1>
-    { isBusy
-      ? <CenteredSpinner size={60} />
-      : !shouldRedirect ? <ItineraryForm teamSettings={teamSettings} /> : null
-    }
-  </DefaultLayout>
+  return (
+    <DefaultLayout>
+      <Spacing pb={ 4 }>
+        <Heading>
+          Genereer looplijst
+        </Heading>
+        <Heading forwardedAs="h2">
+          { teamSettings.name }
+        </Heading>
+      </Spacing>
+      { isBusy
+        ? <CenteredSpinner size={ 60 } />
+        : !shouldRedirect ? <ItineraryForm teamSettings={ teamSettings } /> : null
+      }
+    </DefaultLayout>
+  )
 }
 
 export default CreateItineraryPage
