eb98160c9732732f7ec05e5fbb34b7b5cb54f05e
Vincent Smedinga <v.smedinga@amsterdam.nl>
2020-11-17 11:28:04 +0100

    Extract function

---
+++ src/app/features/settings/utils/filterEmptyPostalCodes.tsx
@@ -0,0 +1,17 @@
+export const filterEmptyPostalCodes = (settings: any) =>
+  ({
+    ...settings,
+    postal_code_ranges: settings.postal_code_ranges?.filter((i: any) => {
+      if (i == null || !Object.keys(i).includes("range_end") || !Object.keys(i).includes("range_start")) {
+        return false
+      }
+      if (!i.range_end) {
+        i.range_end = 1109
+      }
+      if (!i.range_start) {
+        i.range_start = 1000
+      }
+      Object.keys(i).filter(x => ![ "range_start", "range_end" ].includes(x)).map(d => delete i[d])
+      return true
+    }) ?? []
+  })
