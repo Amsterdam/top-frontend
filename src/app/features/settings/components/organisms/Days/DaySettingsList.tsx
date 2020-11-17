e2721b0dd9f53e8aa245c2c85dfa4c89b6aaf0f2
Vincent Smedinga <v.smedinga@amsterdam.nl>
2020-11-17 16:16:47 +0100

    Arrange day settings in a grid

--- src/app/features/settings/components/organisms/Days/DaySettingsList.tsx
+++ src/app/features/settings/components/organisms/Days/DaySettingsList.tsx
@@ -1,7 +1,7 @@
 import React, { FC } from "react"
 import { Link, RouteComponentProps } from "@reach/router"
 import styled from "styled-components"
-import { Heading, themeColor } from "@amsterdam/asc-ui"
+import { Heading, themeSpacing } from "@amsterdam/asc-ui"
 
 import to from "app/features/shared/routing/to"
 import Spacing from "app/features/shared/components/atoms/Spacing/Spacing"
@@ -9,15 +9,10 @@ import DefaultLayout from "app/features/shared/components/layouts/DefaultLayout/
 import CenteredSpinner from "../../../../shared/components/atoms/CenteredSpinner/CenteredSpinner"
 import DaySettings from "app/features/settings/components/organisms/Days/DaySettings"
 
-const Ul = styled.ul`
-  padding: 0;
-  list-style: none;
-  border-top: 1px solid ${ themeColor("tint", "level4") };
-`
-
-const Li = styled.li`
-  padding: 0;
-  border-bottom: 1px solid ${ themeColor("tint", "level4") };
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(512px, 1fr));
+  gap: ${ themeSpacing(4) };
 `
 
 type Props = {
@@ -34,19 +29,21 @@ const DaySettingsList: FC<RouteComponentProps<Props>> = ({ teamSettings, postCod
     <DefaultLayout>
       <Heading>Plan looplijsten</Heading>
       <Heading forwardedAs="h2">{ teamSettings.name }</Heading>
-      <Spacing pb={ 4 }>
+      <Spacing pb={ 8 }>
         <Link to={ to("/team-settings") }>
           Kies een ander team
         </Link>
       </Spacing>
-      <Ul>
+      <Grid>
         { teamSettings.day_settings_list.map(daySettings => (
-          <Li key={ daySettings.id }>
-            <DaySettings teamSettings={ teamSettings } daySettingsId={ daySettings.id }
-                         postCodeRangesPresets={ postCodeRangesPresets } />
-          </Li>
+          <DaySettings
+            key={ daySettings.id }
+            teamSettings={ teamSettings }
+            daySettingsId={ daySettings.id }
+            postCodeRangesPresets={ postCodeRangesPresets }
+          />
         )) }
-      </Ul>
+      </Grid>
     </DefaultLayout>
   )
 }
