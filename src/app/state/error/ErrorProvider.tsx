00c30757839224474a412cb5f951a5f523ac335d
Gilles Wittenberg <mail@gilleswittenberg.com>
2020-11-17 10:55:54 +0100

    Update ErrorProvider.tsx

--- src/app/state/error/ErrorProvider.tsx
+++ src/app/state/error/ErrorProvider.tsx
@@ -1,23 +1,33 @@
 import React, { createContext, useCallback, useState } from "react"
 
+type Severity = "MESSAGE" | "WARNING" | "ERROR"
+
 type Context = {
   error?: string
-  setError: (string?: string) => void
+  severity?: Severity
+  setError: (string?: string, severity?: Severity) => void
   clearError: () => void
 }
 
 export const ErrorContext = createContext<Context>({
   error: undefined,
-  setError: (string?: string) => {},
+  severity: undefined,
+  setError: (string?: string, severity?: Severity) => {},
   clearError: () => {}
 })
 
 const ErrorProvider: React.FC = ({ children }) => {
-  const [error, setError] = useState<string|undefined>(undefined)
+  const [error, setErrorMessage] = useState<string|undefined>(undefined)
+  const [severity, setSeverity] = useState<Severity|undefined>(undefined)
+  const setError = (string?: string, severity?: Severity) => {
+    setErrorMessage(string)
+    setSeverity(sevirity)
+  }
+  
   const clearError = useCallback(() => setError(undefined), [ setError ])
 
   return (
-    <ErrorContext.Provider value={{ error, setError, clearError }}>
+    <ErrorContext.Provider value={{ error, severity, setError, clearError }}>
       { children }
     </ErrorContext.Provider>
   )
