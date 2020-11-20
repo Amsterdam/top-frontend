6cbd52eece9d4387485dfcc9c4391cc64b30cf18
Vincent Smedinga <v.smedinga@amsterdam.nl>
2020-11-20 09:43:53 +0100

    Add initial test for formatDate

---
+++ src/app/features/shared/utils/formatDate.test.ts
@@ -0,0 +1,11 @@
+import formatDate from "./formatDate"
+
+describe("formatDate", () => {
+  it("day, month, year", () => {
+    expect(formatDate("11-11-2022")).toBe("11 nov 2022")
+  })
+
+  it("weekday day, month, year", () => {
+    expect(formatDate("11-11-2022", true)).toBe("vr 11 nov 2022")
+  })
+})
