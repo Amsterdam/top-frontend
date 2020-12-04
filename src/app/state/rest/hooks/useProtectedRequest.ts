4c6d3577c7cc211980989d5f390c887e586493b7
Maurice Guikema <mguikema@gmail.com>
2020-12-04 15:09:11 +0100

    fix: kc token update protected

---
+++ src/app/state/rest/hooks/useProtectedRequest.ts
@@ -0,0 +1,25 @@
+import { useCallback } from "react"
+import useKeycloak from "app/state/auth/keycloak/useKeycloak"
+import useRequest from "./useRequest"
+
+type Method = "get" | "post" | "put" | "patch" | "delete"
+export default () => {
+  const keycloak = useKeycloak()
+  const request = useRequest()
+
+  return useCallback(async (method: Method, url: string, data?: unknown, additionalHeaders = {}) => {
+    const isUpdated = await keycloak.updateToken(30)
+    if (isUpdated) console.log("Keycloak token updated")
+    const headers = {
+      Authorization: `Bearer ${ keycloak.token }`,
+      ...additionalHeaders
+    }
+    const response = await request(
+      method,
+      url,
+      data,
+      headers
+    )
+    return response
+  }, [keycloak, request])
+}
