d069294482656cf5b028da436a71132dcc7d3504
Vincent Smedinga <v.smedinga@amsterdam.nl>
2020-12-11 11:11:02 +0100

    Make labels column a bit more fluid

--- src/app/features/cases/components/organisms/CaseDetail/CaseDetailSectionStyles.tsx
+++ src/app/features/cases/components/organisms/CaseDetail/CaseDetailSectionStyles.tsx
@@ -31,7 +31,7 @@ export const SectionRow = styled.div`
 
 export const Grid = styled.div`
   display: grid;
-  grid-template-columns: 160px auto;
+  grid-template-columns: minmax(140px, 1fr) 3fr;
   grid-gap: ${ themeSpacing(3) } ${ themeSpacing(4) };
   place-items: baseline start;
 `
