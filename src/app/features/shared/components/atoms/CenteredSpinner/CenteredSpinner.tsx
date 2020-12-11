fccbd494d0fd5161ad704f31cf64bf47187fa1de
Vincent Smedinga <v.smedinga@amsterdam.nl>
2020-12-11 16:50:10 +0100

    Allow explaining the user why a spinner is being shown

--- src/app/features/shared/components/atoms/CenteredSpinner/CenteredSpinner.tsx
+++ src/app/features/shared/components/atoms/CenteredSpinner/CenteredSpinner.tsx
@@ -1,9 +1,37 @@
+import React, { FC } from "react"
 import styled from "styled-components"
 import { Spinner } from "@amsterdam/asc-ui"
+import Spacing from "app/features/shared/components/atoms/Spacing/Spacing"
 
-export default styled(Spinner)`
+type Props = {
+  explanation?: string
+  size?: number
+}
+
+const CenterInViewport = styled.div`
   position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%)
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 `
+
+const P = styled.p`
+  margin: 0;
+  font-weight: 500;
+`
+
+const CenteredSpinner: FC<Props> = ({ size, explanation }) => (
+    <CenterInViewport>
+      <Spacing pb={ 4 }>
+        <Spinner size={ size } />
+      </Spacing>
+      { explanation && <P>{ explanation }</P> }
+    </CenterInViewport>
+  )
+
+export default CenteredSpinner
