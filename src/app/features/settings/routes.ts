aea213fc2251910a22ff7068c67fe9dca15a1e93
Vincent Smedinga <v.smedinga@amsterdam.nl>
2020-12-14 16:34:05 +0100

    Delete old settings page

--- src/app/features/settings/routes.ts
+++ src/app/features/settings/routes.ts
@@ -1,10 +1,8 @@
-import SettingsPage from "./components/pages/SettingsPage"
 import TeamSettingsListPage from "./components/pages/TeamSettingsListPage"
 import TeamSettingsDaysPage from "./components/pages/TeamSettingsDaysPage"
 import DaySettingsPage from "./components/pages/DaySettingsPage"
 
 const routes = {
-  "/settings": SettingsPage,
   "/team-settings": TeamSettingsListPage,
   "/team-settings/:teamSettingsId": TeamSettingsDaysPage,
   "/team-settings/:teamSettingsId/:daySettingsId": DaySettingsPage
