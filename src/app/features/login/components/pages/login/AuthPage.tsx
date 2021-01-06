6f6aafb5eb18cce1fba0d850efe6335f494e93a8
Vincent Smedinga <v.smedinga@amsterdam.nl>
2021-01-06 15:06:12 +0100

    Explain not being authorised

--- src/app/features/login/components/pages/login/AuthPage.tsx
+++ src/app/features/login/components/pages/login/AuthPage.tsx
@@ -1,10 +1,11 @@
 import React from "react"
-import { Heading, Alert } from "@amsterdam/asc-ui"
+import { Alert, Heading, Paragraph } from "@amsterdam/asc-ui"
 
 import DefaultLayout from "app/features/shared/components/layouts/DefaultLayout/DefaultLayout"
 import DefinitionList from "app/features/shared/components/molecules/DefinitionList/DefinitionList"
 import useKeycloak from "app/state/auth/keycloak/useKeycloak"
 import { useIsAuthorized } from "app/state/rest/"
+import Spacing from "app/features/shared/components/atoms/Spacing/Spacing"
 
 const AuthPage: React.FC = () => {
   const keycloak = useKeycloak()
@@ -15,15 +16,31 @@ const AuthPage: React.FC = () => {
     Naam: keycloak.tokenParsed?.name ?? "–",
     "E-mail": keycloak.tokenParsed?.email ?? "–",
     Gebruikersnaam: keycloak.tokenParsed?.preferred_username ?? "–",
-    "Keycloak groepen": keycloak.realmAccess?.roles.join(", ") ?? "–"
+    Groepen: keycloak.realmAccess?.roles.join(", ") ?? "–"
   }
 
   return (
     <DefaultLayout>
-      <Heading as="h2">Keycloak gebruiker</Heading>
       { showUnauthorized &&
-        <Alert level="error">Je bent niet geauthorizeerd. Waarschijnlijk staan de Keycloak groepen gekoppeld aan je ADW account niet goed ingesteld.</Alert>
+      <>
+        <Spacing pb={ 4 }>
+          <Alert level="error">
+            <Paragraph>
+              Helaas, je hebt geen toegang.
+            </Paragraph>
+          </Alert>
+        </Spacing>
+        <Paragraph>
+          ‘Toezicht op pad’ is een applicatie voor toezichthouders van de afdeling Wonen.
+          Je bent op dit moment niet geautoriseerd om deze applicatie te gebruiken.
+        </Paragraph>
+        <Paragraph>
+          Neem contact op met je teamleider of de afdeling Wonen om na te gaan of je toegang kunt krijgen.
+          De juiste Keycloak-groepen worden dan toegevoegd aan je ADW-account.
+        </Paragraph>
+      </>
       }
+      <Heading forwardedAs="h2">Keycloak-account</Heading>
       <DefinitionList values={ values } />
     </DefaultLayout>
   )
