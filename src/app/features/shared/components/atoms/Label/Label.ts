c1a2b575048fa909601e3d1d23272d3dd51221ca
Vincent Smedinga <v.smedinga@amsterdam.nl>
2021-02-09 10:06:44 +0100

    Break long words in labels

--- src/app/features/shared/components/atoms/Label/Label.ts
+++ src/app/features/shared/components/atoms/Label/Label.ts
@@ -4,6 +4,7 @@ import { themeColor } from "@amsterdam/asc-ui"
 const Label = styled.label`
   color: ${ themeColor("tint", "level4") };
   font-weight: 500;
+  word-break: break-word;
 `
 
 export default Label
