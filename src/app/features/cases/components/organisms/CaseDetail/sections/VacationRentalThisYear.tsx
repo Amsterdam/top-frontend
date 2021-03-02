2da3d36e0c35704ee9876a911848daaec407410d
Vincent Smedinga <v.smedinga@amsterdam.nl>
2021-03-02 10:25:26 +0100

    Only show check in details if requested

--- src/app/features/cases/components/organisms/CaseDetail/sections/VacationRentalThisYear.tsx
+++ src/app/features/cases/components/organisms/CaseDetail/sections/VacationRentalThisYear.tsx
@@ -1,4 +1,7 @@
 import React, { FC } from "react"
+import styled from "styled-components"
+
+import { themeColor, themeSpacing } from "@amsterdam/asc-ui"
 
 import { useAllPermitCheckmarks, useCase } from "app/state/rest"
 import formatBoolean from "app/features/shared/utils/formatBoolean"
@@ -6,7 +9,7 @@ import formatDate from "app/features/shared/utils/formatDate"
 import Label from "app/features/shared/components/atoms/Label/Label"
 import Value from "app/features/shared/components/atoms/Value/Value"
 
-import { getBagId } from "app/features/cases/components/organisms/CaseDetail/utils"
+import { getBagId } from "../utils"
 import CaseDetailSection from "../CaseDetailSection"
 import { Grid, Hr, TwoColumns } from "../CaseDetailSectionStyles"
 
@@ -14,10 +17,24 @@ type Props = {
   caseId: string
 }
 
+const Details = styled.details`
+  grid-column: span 2;
+  justify-self: stretch;
+`
+
+const Summary = styled.summary`
+  color: ${ themeColor("primary") };
+
+  details[open] > & {
+    margin-bottom: ${ themeSpacing(4) };
+  }
+`
+
 const VacationRentalThisYear: FC<Props> = ({ caseId }) => {
   const { data: caseData } = useCase(caseId)
   const bagId = getBagId(caseData!)
   const { data: decos, isBusy } = useAllPermitCheckmarks(bagId!, { lazy: !bagId })
+  const verhuur = decos?.vakantieverhuur_meldingen ?? []
 
   if (!caseData) {
     return null
@@ -32,35 +49,46 @@ const VacationRentalThisYear: FC<Props> = ({ caseId }) => {
     >
       <Grid>
         <Label>Nachten verhuurd</Label>
-        <Value value={ decos?.vakantieverhuur_meldingen.rented_days_count } />
+        <Value>
+          <strong>{ verhuur.rented_days_count }</strong>
+        </Value>
         <Label>Vandaag verhuurd</Label>
-        <Value value={ formatBoolean(decos?.vakantieverhuur_meldingen.is_rented_today) } />
+        <Value value={ formatBoolean(verhuur.is_rented_today) } />
         <Label>Nachten gepland</Label>
-        <Value value={ decos?.vakantieverhuur_meldingen.planned_days_count } />
-        <TwoColumns>
-          <Hr />
-        </TwoColumns>
-        {
-          decos?.vakantieverhuur_meldingen.meldingen.map((melding: Components.Schemas.VakantieverhuurMelding) => {
-            const firstDay = new Date(melding.check_in_date)
-            const lastDay = new Date(melding.check_out_date)
-            const nightsRented = (lastDay.getTime() - firstDay.getTime()) / 8.64e+7
+        <Value value={ verhuur.planned_days_count } />
+        { verhuur.rented_days_count > 0 &&
+        <>
+          <TwoColumns>
+            <Hr />
+          </TwoColumns>
+          <Details>
+            <Summary>Alle meldingen</Summary>
+            <Grid>
+              {
+                verhuur.meldingen.map((melding: Components.Schemas.VakantieverhuurMelding) => {
+                  const checkIn = new Date(melding.check_in_date)
+                  const checkOut = new Date(melding.check_out_date)
+                  const nightsRented = (checkOut.getTime() - checkIn.getTime()) / 8.64e+7
 
-            return (
-              <>
-                <TwoColumns>
-                  <strong>
-                    { melding.is_afmelding ? "Afmelding " : "Melding " }
-                    { nightsRented } nachten
-                  </strong>
-                </TwoColumns>
-                <Label>Check in</Label>
-                <Value value={ formatDate(melding.check_in_date) } />
-                <Label>Check out</Label>
-                <Value value={ formatDate(melding.check_out_date) } />
-              </>
-            )
-          })
+                  return (
+                    <>
+                      <TwoColumns>
+                        <strong>
+                          { melding.is_afmelding ? "Afmelding " : "Melding " }
+                          { nightsRented } nachten
+                        </strong>
+                      </TwoColumns>
+                      <Label>Check in</Label>
+                      <Value value={ formatDate(melding.check_in_date) } />
+                      <Label>Check out</Label>
+                      <Value value={ formatDate(melding.check_out_date) } />
+                    </>
+                  )
+                })
+              }
+            </Grid>
+          </Details>
+        </>
         }
       </Grid>
     </CaseDetailSection>
