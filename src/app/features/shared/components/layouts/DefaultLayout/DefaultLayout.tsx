ffcfaddf63e85a06013abf0b49703e8102203094
Vincent Smedinga <v.smedinga@amsterdam.nl>
2021-03-02 15:34:46 +0100

    Format code

--- src/app/features/shared/components/layouts/DefaultLayout/DefaultLayout.tsx
+++ src/app/features/shared/components/layouts/DefaultLayout/DefaultLayout.tsx
@@ -1,21 +1,15 @@
 import React from "react"
+import styled from "styled-components"
+
 import { Button, Header, themeColor } from "@amsterdam/asc-ui"
 import { Logout } from "@amsterdam/asc-assets"
 import { Link } from "@reach/router"
+
 import useKeycloak from "app/state/auth/keycloak/useKeycloak"
-
-import styled from "styled-components"
-
 import to from "app/features/shared/routing/to"
-import Navigation from "../../organisms/Navigation/Navigation"
 import Spacing from "app/features/shared/components/atoms/Spacing/Spacing"
 import ErrorDisplay from "../../organisms/ErrorDisplay/ErrorDisplay"
-
-const StyledLink = styled(Link)`
-  color ${ themeColor("tint", "level6") };
-  text-decoration: none;
-  margin-right: 8px
-`
+import Navigation from "../../organisms/Navigation/Navigation"
 
 const HeaderWrap = styled.div`
   position: fixed;
@@ -24,33 +18,42 @@ const HeaderWrap = styled.div`
   z-index: 500;
 `
 
+const StyledLink = styled(Link)`
+  color ${ themeColor("tint", "level6") };
+  text-decoration: none;
+  margin-right: 8px
+`
+
 type Props = {}
 
 const DefaultLayout: React.FC<Props> = ({ children }) => {
   const { logout } = useKeycloak()
 
-  return <div>
-    <HeaderWrap>
-      <ErrorDisplay />
-      <Header
-        title="Toezicht op pad"
-        homeLink={ to("/") }
-        fullWidth={ true }
-        navigation={
-          <StyledLink as="a" onClick={ logout }>
-            <Button
-              as="span"
-              variant="blank"
-              iconLeft={ <Logout /> }>Log uit
-            </Button>
-          </StyledLink>
-        }
-      />
-    </HeaderWrap>
-    <Navigation />
-    <Spacing p={ 4 }>
-      { children }
-    </Spacing>
-  </div> }
+  return (
+    <div>
+      <HeaderWrap>
+        <ErrorDisplay />
+        <Header
+          title="Toezicht op pad"
+          homeLink={ to("/") }
+          fullWidth={ true }
+          navigation={
+            <StyledLink as="a" onClick={ logout }>
+              <Button
+                as="span"
+                variant="blank"
+                iconLeft={ <Logout /> }>Log uit
+              </Button>
+            </StyledLink>
+          }
+        />
+      </HeaderWrap>
+      <Navigation />
+      <Spacing p={ 4 }>
+        { children }
+      </Spacing>
+    </div>
+  )
+}
 
 export default DefaultLayout
