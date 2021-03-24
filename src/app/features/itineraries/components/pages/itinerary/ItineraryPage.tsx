Amsterdam/top-frontend

commit 4b4f0d6b1369fbe4dcc3db05f09b78fbf2790c71

fix: wz-99 disable google maps button when all items are visited

src/app/features/itineraries/components/pages/itinerary/ItineraryPage.tsx
@@ -86,7 +86,7 @@ const ItineraryPage: React.FC<RouteComponentProps<Props>> = ({ itineraryId }) =>
 
   const teamMemberUsers = useMemo(() => data?.team_members.map(member => member.user) ?? [], [ data ])
   const teamMemberNames = useMemo(() => teamMemberUsers?.map(user => user.full_name).join(", "), [ teamMemberUsers ])
-  const cases = useMemo(() => items?.map(item => item.case.bwv_data) ?? [], [ items ])
+  const cases = useMemo(() => items?.filter(item => item.visits.length === 0).map(item => item.case.bwv_data) ?? [], [ items ])
 
   const clipboardText = cases.map(itineraryToClipboardText).join("\n")
   const onClickClipboard = () => setShowDialog(false)

src/app/features/itineraries/components/molecules/MapsButton/MapsButton.tsx
@@ -32,7 +32,7 @@ const MapsButton: FC<Props> = ({ cases }) => {
     window.open(href, "_blank")
   }
   return (
-    <StyledButton variant="blank" iconLeft={ <Location /> } onClick={ onClick }>
+    <StyledButton disabled={cases.length === 0} variant="blank" iconLeft={ <Location /> } onClick={ onClick }>
       <ResponsiveText text={ ["Maps", "Bekijk op Google Maps"] } />
     </StyledButton>
   )

commit ed2c6779088a6899ab185ef01ac18b36ce6c44c0

Included visited addresses when copying itinerary items
This is a regression from 4b4f0d6b1369fbe4dcc3db05f09b78fbf2790c71

src/app/features/itineraries/components/pages/itinerary/ItineraryPage.tsx
@@ -102,7 +102,9 @@ const ItineraryPage: React.FC<RouteComponentProps<Props>> = ({ itineraryId }) =>
 
   const teamMemberUsers = useMemo(() => itinerary?.team_members.map(member => member.user) ?? [], [ itinerary ])
   const teamMemberNames = useMemo(() => teamMemberUsers?.map(user => user.full_name).join(", "), [ teamMemberUsers ])
-  const cases = useMemo(() => items?.filter(item => item.visits.length === 0).map(item => item.case.data) ?? [], [ items ])
+
+  const cases = useMemo(() => items?.map(item => item.case.data) ?? [], [ items ])
+  const casesWithoutVisits = useMemo(() => items?.filter(item => item.visits.length === 0).map(item => item.case.data) ?? [], [ items ])
 
   const clipboardText = cases.map(itineraryToClipboardText).join("\n")
   const onClickClipboard = () => setShowDialog(false)
@@ -144,7 +146,7 @@ const ItineraryPage: React.FC<RouteComponentProps<Props>> = ({ itineraryId }) =>
         </div>
         <ColumnWrap border={ true }>
           <Left>
-            <MapsButton cases={ cases } />
+            <MapsButton cases={ casesWithoutVisits } />
           </Left>
           <Right>
             <StyledButton onClick={ () => navigate(to("/lijst/:itineraryId/suggesties", { itineraryId })) }