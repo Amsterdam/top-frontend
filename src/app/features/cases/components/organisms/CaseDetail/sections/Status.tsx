daed24360cb8b06bdfaa12d02fa984a8779a5dd7
Remco Hoff <remco@code010.nl>
2021-04-12 16:18:41 +0200

    Moved the styling to @amsterdam/wonen-ui

--- src/app/features/cases/components/organisms/CaseDetail/sections/Status.tsx
+++ src/app/features/cases/components/organisms/CaseDetail/sections/Status.tsx
@@ -1,35 +1,11 @@
-import React, { FC } from "react"
+import { FC } from "react"
 import { TimelineEvents } from "@amsterdam/wonen-ui"
 import useGroupedCaseEvents from "../hooks/useGroupedCaseEvents"
 import CaseDetailSection from "app/features/cases/components/organisms/CaseDetail/CaseDetailSection"
-import styled from "styled-components"
-import { themeSpacing } from "@amsterdam/asc-ui"
 
 type Props = {
   caseId: string
 }
-
-const Div = styled.div`
-  > div[role="button"] {
-    position: relative;
-    margin: 0 -${ themeSpacing(3) } ${ themeSpacing(5) };
-    padding: 0 ${ themeSpacing(3) };
-
-    &:last-child {
-      > div:nth-child(2) {
-        > div:first-child {
-          &:after {
-            display: none;
-          }
-        }
-      }
-    }
-
-    button {
-      outline: none;
-    }
-  }
-`
 
 const Status: FC<Props> = ({ caseId }) => {
   const [ timelineEvents ] = useGroupedCaseEvents(caseId)
@@ -39,9 +15,9 @@ const Status: FC<Props> = ({ caseId }) => {
       title="Status"
       dataSource="Zaaksysteem">
       { timelineEvents !== undefined &&
-      <Div>
-        <TimelineEvents items={ timelineEvents as any } />
-      </Div>
+      <div>
+        <TimelineEvents items={ timelineEvents as any } spacingHorizontal={3} />
+      </div>
       }
     </CaseDetailSection>
   )
