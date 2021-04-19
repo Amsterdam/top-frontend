6083866bd7301abe075c80f01c2dd738c30927aa
Maurice Guikema <mguikema@gmail.com>
2021-04-19 10:48:33 +0200

    fix: weekday diff

--- src/app/features/itineraries/components/organisms/ItineraryForm/getDaySettingsOptions.ts
+++ src/app/features/itineraries/components/organisms/ItineraryForm/getDaySettingsOptions.ts
@@ -1,5 +1,6 @@
 export const getDaySettingsOptions = (teamSettings: Components.Schemas.TeamSettings) => {
-  const daySettingsListSpecific = teamSettings.day_settings_list.filter(ds => ds.week_days?.includes(new Date().getDay()))
+  const weekDay = new Date().getDay() === 0 ? 6 : new Date().getDay() - 1
+  const daySettingsListSpecific = teamSettings.day_settings_list.filter(ds => ds.week_days?.includes(weekDay))
   const daySettingsListRest = teamSettings.day_settings_list.filter(ds => ds.week_days === null || ds.week_days.length === 0)
 
   return (daySettingsListSpecific.length > 0 ? daySettingsListSpecific : daySettingsListRest).map(ds => ({
