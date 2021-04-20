ba582b9c0824497f522fd338cf79cd58c0c308a8
Vincent Smedinga <v.smedinga@amsterdam.nl>
2021-04-20 10:11:30 +0200

    Display available and max itineraries in itinerary form

--- src/app/features/itineraries/components/organisms/ItineraryForm/getDaySettingsOptions.ts
+++ src/app/features/itineraries/components/organisms/ItineraryForm/getDaySettingsOptions.ts
@@ -1,10 +1,11 @@
 export const getDaySettingsOptions = (teamSettings: Components.Schemas.TeamSettings) => {
   const weekDay = new Date().getDay() === 0 ? 6 : new Date().getDay() - 1
   const daySettingsListSpecific = teamSettings.day_settings_list.filter(ds => ds.week_days?.includes(weekDay))
-  const daySettingsListRest = teamSettings.day_settings_list.filter(ds => ds.week_days === null || ds.week_days.length === 0)
+  const daySettingsListGeneral = teamSettings.day_settings_list.filter(ds => ds.week_days === null || ds.week_days.length === 0)
 
-  return (daySettingsListSpecific.length > 0 ? daySettingsListSpecific : daySettingsListRest).map(ds => ({
-    label: ds.name,
-    daySettingsId: ds.id
-  }))
+  return (daySettingsListSpecific.length > 0 ? daySettingsListSpecific : daySettingsListGeneral).map(ds => ({
+      label: `${ ds.name } (${ (ds.max_use_limit ?? 0) - ds.used_today_count } van ${ ds.max_use_limit } beschikbaar)`,
+      daySettingsId: ds.id
+    })
+  )
 }
