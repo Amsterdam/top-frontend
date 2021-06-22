fecd3185ecf8d9a946f0ce81e02d1314100c5bd8
Gilles Wittenberg <mail@gilleswittenberg.com>
2021-06-22 13:58:20 +0200

    Fix ENV names

--- src/sentry/init.ts
+++ src/sentry/init.ts
@@ -1,10 +1,10 @@
 import * as Sentry from "@sentry/browser"
 
 const initSentry = () => {
-  if (process.env.REACT_APP_SENTY_ENV && process.env.REACT_APP_SENTY_ENV !== "development") {
+  if (process.env.REACT_APP_SENTRY_ENV && process.env.REACT_APP_SENTRY_ENV !== "development") {
     Sentry.init({
       dsn: "https://889f135ead3a4523aa463e38e87e6a92@sentry.data.amsterdam.nl/47",
-      environment: process.env.REACT_APP_SENTY_ENV,
+      environment: process.env.REACT_APP_SENTRY_ENV,
       release: process.env.REACT_APP_GIT_COMMIT_HASH
     })
   }
