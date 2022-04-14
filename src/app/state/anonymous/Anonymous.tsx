98f895216c0576a46306ad022402f30fc2cfcd46
remyvdwereld <86827854+remyvdwereld@users.noreply.github.com>
2022-04-14 13:23:08 +0200

    WZ-1927

--- src/app/state/anonymous/Anonymous.tsx
+++ src/app/state/anonymous/Anonymous.tsx
@@ -4,8 +4,24 @@ import { AnonymousContext } from "./AnonymousProvider"
 const Anonymous: FC = () => {
   const { isAnonymous } = useContext(AnonymousContext)
   return isAnonymous
-    ? <style>{ ".anonymous, .anonymous a { color: transparent; text-shadow: 0 0 15px rgba(0, 0, 0, 0.5); }" }</style>
-    : null
+    ? (
+    <style>
+      {
+        `.anonymous, .anonymous a {
+            color: transparent;
+            text-shadow: 0 0 15px rgba(0, 0, 0, 0.5);
+          }
+          .blur {
+            -webkit-filter: blur(5px);
+            -moz-filter: blur(5px);
+            -o-filter: blur(5px);
+            -ms-filter: blur(5px);
+            filter: blur(5px);
+          }
+        `
+      }
+    </style>
+    ) : null
 }
 
 export default Anonymous
