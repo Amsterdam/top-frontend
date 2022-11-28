1e465b5245a1d419d838be3db92efb5145340f0b
remyvdwereld <86827854+remyvdwereld@users.noreply.github.com>
2022-11-28 15:19:37 +0100

    64004

--- src/app/features/itineraries/utils/mapUsersToLabel.ts
+++ src/app/features/itineraries/utils/mapUsersToLabel.ts
@@ -7,7 +7,7 @@ const mapUsersToLabel = (users: Components.Schemas.User[]) => {
   const userList: User[] = users.map((user: any) => {
     const { last_name, first_name, full_name } = user
     const newUser: User = { ...user }
-    newUser.label = last_name ? `${ last_name }, ${ first_name }` : full_name
+    newUser.label = first_name && last_name ? `${ first_name } ${ last_name }` : full_name
     return newUser
 })
   return userList.sort((a: User, b: User) => a.label.localeCompare(b.label))
