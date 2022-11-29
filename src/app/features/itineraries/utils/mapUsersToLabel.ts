988b443f3459d671c5d2764bf7a1e2b89c0ad59d
remyvdwereld <86827854+remyvdwereld@users.noreply.github.com>
2022-11-29 13:45:10 +0100

    Updated label

--- src/app/features/itineraries/utils/mapUsersToLabel.ts
+++ src/app/features/itineraries/utils/mapUsersToLabel.ts
@@ -4,15 +4,21 @@ type User = Components.Schemas.User & {
 }
 
 export const createUserWithLabel = (user: Components.Schemas.User) => {
-  const { first_name, full_name } = user
+  const { first_name, last_name, full_name } = user
   const newUser: User = { ...user }
   let label = full_name
-  if (first_name) {
+  if (first_name && last_name) {
+    // full_name has no dot in name.
+    if (full_name.indexOf(".") === -1) {
+      label = `${ first_name } ${ last_name }`
+    }
+    // full_name has dots so replace initials by firstname.
     if (full_name.charAt(1) === "." && full_name.charAt(0) === first_name.charAt(0)) {
       const initials = full_name.substring(0, full_name.lastIndexOf(".") + 1)
       label = full_name.replace(initials, first_name)
     }
   }
+  // Remove space at the beginning of the label
   if (label.charAt(0) === " ") {
     label = label.slice(1)
   }
