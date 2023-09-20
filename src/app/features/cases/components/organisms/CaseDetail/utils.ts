353be19e5ed1bbd1e04e37e8a78d171af68dc424
remyvdwereld <86827854+remyvdwereld@users.noreply.github.com>
2023-09-20 13:29:35 +0200

    Styling updated

--- src/app/features/cases/components/organisms/CaseDetail/utils.ts
+++ src/app/features/cases/components/organisms/CaseDetail/utils.ts
@@ -1,4 +1,4 @@
-import { BagData, BagDataError, BrkData, BrkDataError, Case, Address } from "app/features/types"
+import { BagData, BagDataError, Case, Address } from "app/features/types"
 import displayAddress from "app/features/shared/utils/displayAddress"
 
 export const getAddress = (address: Address) => displayAddress(address.street_name, address.number, address.suffix_letter || undefined, address.suffix || undefined)
@@ -8,29 +8,6 @@ export const getBagId = (caseData: Case) => {
   const bagData = caseData?.bag_data as BagData
 
   return hasBagData ? (bagData.verblijfsobjectidentificatie ?? bagData.ligplaatsidentificatie) : undefined
-}
-
-export const getEigenaar = (caseData: Case) => {
-  const hasBrkData = (caseData.brk_data as BrkDataError).error === undefined
-
-  if (!hasBrkData) {
-    return undefined
-  }
-
-  const brkData = caseData.brk_data as BrkData
-
-  if (brkData.results.length) {
-    const rechten = brkData.results.map(result => (
-      <>
-        { result?.cultuurcode_bebouwd?.omschrijving && (
-          <b>{ result?.cultuurcode_bebouwd?.omschrijving }</b>
-        )}
-        <ul>{result?.rechten?.map(recht => <li>{ recht._display}</li>)}</ul>
-      </>
-    ))
-    return rechten
-  }
-  return undefined
 }
 
 export const isNullish = (a: any): a is undefined | null => a === undefined || a === null || a === ""
