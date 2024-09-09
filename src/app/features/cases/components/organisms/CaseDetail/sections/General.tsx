5ec6c4032da7446b7035a94d803a2a6fc4fe90d5
Remy van der Wereld <r.van.der.wereld@amsterdam.nl>
2024-09-09 10:52:18 +0200

    Remove fraudprediction

--- src/app/features/cases/components/organisms/CaseDetail/sections/General.tsx
+++ src/app/features/cases/components/organisms/CaseDetail/sections/General.tsx
@@ -1,19 +1,11 @@
-import React, { FC } from "react"
+import React from "react"
 import styled from "styled-components"
-import { Link } from "react-router-dom"
 import { Heading, themeSpacing } from "@amsterdam/asc-ui"
 import { CaseIdDisplay } from "@amsterdam/wonen-ui"
-
-import { useCase, useDaySettings } from "app/state/rest"
-
-import FraudProbability from "app/features/shared/components/atoms/FraudProbability/FraudProbability"
+import { useCase } from "app/state/rest"
 import Label from "app/features/shared/components/atoms/Label/Label"
 import Value from "app/features/shared/components/atoms/Value/Value"
 import StadiumBadge from "app/features/shared/components/molecules/StadiumBadge/StadiumBadge"
-import FraudPredictionDetailsModal from "app/features/cases/components/organisms/FraudPrediction/FraudPredictionDetailsModal"
-import { useFraudPredictionModal } from "app/features/cases/components/organisms/FraudPrediction/hooks/useFraudPredictionModal"
-import { hideFraudProbability } from "app/features/shared/utils/fraudPredictionPilot"
-
 import { getAddress } from "../utils"
 import {
   StyledAnchor,
@@ -39,10 +31,8 @@ const BadgesRow = styled.div`
   gap: ${ themeSpacing(2) };
 `
 
-const General: FC<Props> = ({ caseId }) => {
+const General: React.FC<Props> = ({ caseId }) => {
   const { data: caseData } = useCase(caseId)
-  const { data: daySettings } = useDaySettings(caseData?.day_settings_id!)
-  const { getUrl: getToFraudPredictionModalUrl } = useFraudPredictionModal()
 
   if (!caseData) {
     return null
@@ -54,8 +44,6 @@ const General: FC<Props> = ({ caseId }) => {
   const hasPriority = (caseData.schedules && caseData.schedules[0]?.priority?.weight >= 0.5) ?? false
   const hasWarrant = (caseData.schedules && caseData.schedules[0]?.priority?.weight >= 1.0) ?? false
   const lastStadiumLabel = caseData.workflows?.length > 0 ? caseData.workflows[0].state.name : undefined
-
-  const fraudPrediction = !caseData.day_settings_id || (daySettings && daySettings.team_settings.fraud_prediction_model) ? caseData.fraud_prediction : undefined
   const hasProject = caseData?.project?.name !== undefined
 
   return (
@@ -79,21 +67,9 @@ const General: FC<Props> = ({ caseId }) => {
             </>
           )}
           { caseData?.subjects && caseData?.subjects.length > 0 && (
-              <>
-                <Label>Onderwerpen</Label>
-                <Value>{ caseData?.subjects.map((subject: { name: string }) => subject.name).join(", ") }</Value>
-              </>
-          )}
-          { fraudPrediction && !hideFraudProbability(caseId, daySettings?.team_settings?.fraudprediction_pilot_enabled) && (
             <>
-              <Label>Voorspelling (bèta)</Label>
-              <Link to={ getToFraudPredictionModalUrl() }>
-                <FraudProbability fraudProbability={ fraudPrediction?.fraud_probability } />
-              </Link>
-              <FraudPredictionDetailsModal
-                title={ address }
-                fraudPrediction={ fraudPrediction! }
-              />
+              <Label>Onderwerpen</Label>
+              <Value>{ caseData?.subjects.map((subject: { name: string }) => subject.name).join(", ") }</Value>
             </>
           )}
         </Grid>
