ec10300be70762425e4b662f88e69c11a86cd248
Remy van der Wereld <r.van.der.wereld@amsterdam.nl>
2025-01-08 10:36:30 +0100

    Refactor SuggestionsPage to use generic Params type for itineraryId

--- src/app/features/itineraries/components/pages/suggestions/SuggestionsPage.tsx
+++ src/app/features/itineraries/components/pages/suggestions/SuggestionsPage.tsx
@@ -6,18 +6,14 @@ import CenteredSpinner from "app/features/shared/components/atoms/CenteredSpinne
 import Suggestions from "./Suggestions"
 import { useGeoLocation } from "app/features/shared/hooks/useGeoLocation/useGeoLocation" 
 
-type Props = {
+type Params = {
   itineraryId: string
 }
 
-// const LAT = "52.3706866"
-// const LNG = "4.8939469"
-
 const SuggestionsPage: React.FC = () => {
-  const { itineraryId } = useParams<Props>()
+  const { itineraryId } = useParams<Params>()
   const { location, isBusy } = useGeoLocation()
-  console.log("BUSY", isBusy)
-  console.log("location", location)
+
   return (
     <DefaultLayout>
       <Heading>Voeg een zaak toe aan je looplijst</Heading>
